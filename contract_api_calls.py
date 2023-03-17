588d088879a72bb4f453b4c236cc4ada7235e9d3
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2023-03-17 15:21:55 -0400

    Updated the deprecated smart-contracts-api url to the new Heroku hosted url

--- contract_api_calls.py
+++ contract_api_calls.py
@@ -3,8 +3,8 @@ import requests
 
 def token_endpoint():
 
-    return requests.get("http://ec2-34-203-42-249.compute-1.amazonaws.com/token").json()
+    return requests.get("https://smart-contracts-api-node.herokuapp.com/token").json()
 
 
 def ttBank_endpoint():
-    return requests.get("http://ec2-34-203-42-249.compute-1.amazonaws.com/ttBank").json()
+    return requests.get("https://smart-contracts-api-node.herokuapp.com/ttBank").json()
